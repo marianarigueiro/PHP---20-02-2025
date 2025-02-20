f093d4453576e55f7a81fd54c438c9034fcd687b
Mariana de O. Rigueiro <166918439+marianarigueiro@users.noreply.github.com>
2025-02-20 16:29:22 -0300

    Add files via upload

---
+++ aulaphp/index.php
@@ -0,0 +1,57 @@
+    <?php 
+       /* $x = 10;
+        $y = 20;
+
+        $z = 1;
+
+        if($z == 1){
+        //Adição
+        $total = $x + $y;
+        echo $total;
+        }elseif($z == 2){
+        //Subtração
+        $total = $x - $y;
+        echo $total;
+        }elseif($z == 3){
+        //Multiplicação
+        $total = $x * $y;
+        echo $total;
+        }else{
+        //Divisão
+        $total = $x / $y;
+        echo $total;
+        } 
+
+        $a = 1;
+
+        while($a <= 10){
+            echo $a;
+            $a = $a + 1;
+        }
+
+        for($s = 1; $s <= 10; $s = $s + 1){
+            echo $s;
+        }
+
+        $y = 0;
+
+
+        do{
+            echo $y;
+            $y = $y + 1;
+        }while($y <= 10); 
+
+        $s = 1;
+
+        for($s <= 10){
+            echo $s;
+            $s = $s * 1; 
+        }*/
+
+        $y = 1;
+
+        do{
+            echo $y;
+            $y = $y + 1;
+        }while($y <= 10); 
+    ?>
